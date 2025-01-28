d1a5df594fbd45f0eb9201be492ee15de66b8f83
poslusznymarcin <marcin.posluszny@microsoft.wsei.edu.pl>
2025-01-28 17:24:19 +0100

    Setup Entity Framework and create entities

---
+++ WhatAndWhen.Data/Entities/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatAndWhen.Data.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Status { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
