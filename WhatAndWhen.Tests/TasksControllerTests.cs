1ea1d1f00d532e335d50d5e4d998280c55b1519b
poslusznymarcin <marcin.posluszny@microsoft.wsei.edu.pl>
2025-01-29 20:44:01 +0100

    Add unit tests for TasksController

---
+++ WhatAndWhen.Tests/TasksControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatAndWhen.Data;
+using WhatAndWhen.Web.Controllers;
+
+
+namespace WhatAndWhen.Tests
+{
+    [TestFixture]
+    public class TasksControllerTests : IDisposable
+    {
+        private TasksController _controller;
+        private WhatAndWhenContext _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<WhatAndWhenContext>()
+                .UseInMemoryDatabase(databaseName: "WhatAndWhenTestDb")
+                .Options;
+
+            _context = new WhatAndWhenContext(options);
+            _context.Tasks.Add(new Data.Entities.Task { Title = "TestTask", Id = 1 });
+            _context.SaveChanges();
+            _controller = new TasksController(_context);
+        }
+
+        [Test]
+        public async Task Index_ReturnsViewResult_WithList()
+        {
+            // Act
+            var result = await _controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            var model = result.Model as List<Data.Entities.Task>;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(1, model.Count);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+            if (_controller != null)
+            {
+
+            }
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
