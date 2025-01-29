f2c6728bbbd54cd12cb5293bdd089f1ce27d9edd
poslusznymarcin <marcin.posluszny@microsoft.wsei.edu.pl>
2025-01-29 18:12:57 +0100

    Add DbContext and initial migration

---
+++ WhatAndWhen.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WhatAndWhen.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<WhatAndWhenContext>
+    {
+        public WhatAndWhenContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WhatAndWhenContext>();
+            // Użyj tego samego connection string, który używasz w appsettings.json
+            optionsBuilder.UseSqlite("Data Source=WhatAndWhen.db");
+            return new WhatAndWhenContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ WhatAndWhen.Data/WhatAndWhenContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WhatAndWhen.Data.Entities;
+using Task = WhatAndWhen.Data.Entities.Task;
+
+namespace WhatAndWhen.Data
+{
+    public class WhatAndWhenContext : DbContext
+    {
+        public WhatAndWhenContext(DbContextOptions<WhatAndWhenContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=WhatAndWhen.db");
+        }
+    }
+}
